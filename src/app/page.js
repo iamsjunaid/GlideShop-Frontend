19c1798b68142f1e992a08837e9f2e0ff173bb9a
Junaid Syed <sjunaid626@gmail.com>
2023-12-15 20:48:10 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,19 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <main className="sm:mx-8">
       <Navbar />
-      this is a next and tailwindcss app.
+      <div className="container flex justify-center mx-auto">
+        <Image
+          src="/images/banner.png"
+          alt="Next.js logo"
+          width={3200}
+          height={200}
+        />
+      </div>
       <Footer />
     </main>
   )
