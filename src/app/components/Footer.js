77323dcf847f048a39c4e7ecb2f292ae6eecf666
Junaid Syed <sjunaid626@gmail.com>
2023-12-15 20:48:04 +0530

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import Image from "next/image"
 
 const Footer = () => {
   return (
     <div>
       <footer className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+        <div className="container px-5 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
           <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
-            <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-              <Image src="/images/logo.png" width={100} height={40} alt="logo" />
+            <a className="flex title-font font-medium items-center md:justify-start justify-center">
+              <img src="/images/logo.png" alt="logo" />
             </a>
-            <p className="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
