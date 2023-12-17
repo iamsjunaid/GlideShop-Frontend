7bdc48c328ca65455190f9bd05b6da5a890232d5
Junaid Syed <sjunaid626@gmail.com>
2023-12-17 15:38:56 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BsCart3 } from 'react-icons/bs'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
@@ -8,7 +9,13 @@ const Navbar = () => {
       <header className="text-gray-600 body-font shadow-md mb-4 sticky top-0">
         <div className="container mx-auto py-8 md:py-0 flex flex-wrap flex-col md:flex-row items-center">
           <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="/">
-            <img src="/images/logo.png" alt="logo" className="w-32 rounded-lg" />
+            <Image
+              src="/images/logo.png"
+              alt="logo"
+              width={500}
+              height={187}
+              className="w-32 rounded-lg"
+            />
           </Link>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
             <Link href="/tshirts" className="mr-5 hover:text-gray-900">
@@ -19,7 +26,7 @@ const Navbar = () => {
             <a className="mr-5 hover:text-gray-900">Third Link</a>
             <a className="mr-5 hover:text-gray-900">Fourth Link</a>
           </nav>
-          <BsCart3 className="absolute top-8 right-4 sm:static w-6 h-6 ml-2" />
+          <BsCart3 className="absolute top-8 right-4 md:static w-6 h-6 ml-2" />
         </div>
       </header>
     </div>
