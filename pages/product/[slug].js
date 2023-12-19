614d946cf6fe3cc6c5a4136be8a9b75e14910521
Junaid Syed <sjunaid626@gmail.com>
2023-12-19 14:40:57 +0530

    display a success message

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -194,6 +194,10 @@ const Post = () => {
             <div className="flex mt-4 gap-2">
               <input id="inputPincode" type="text" placeholder="Enter Your Pincode" className="w-3/4 bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-[#9a4747] focus:bg-transparent border border-gray-300 focus:border-[#9a4747] text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
               <button className="flex ml-auto text-white bg-[#9a4747] border-0 py-2 px-6 focus:outline-none hover:underline rounded" onClick={checkAvailibility}>Check</button>
+            </div>
+            {availibility === true && (
+              <p className="text-green-500">Delievery is available at your location.</p>
+            )}
           </div>
         </div>
       </div>
