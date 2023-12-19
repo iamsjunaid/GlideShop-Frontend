6baacb83af3b614e7a8deb23e62fd39e08d14502
Junaid Syed <sjunaid626@gmail.com>
2023-12-19 21:31:54 +0530

    create saveCart method

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,14 @@ function MyApp ({ Component, pageProps }) {
   const [cart, setCart] = useState({})
   const [subTotal, setSubTotal] = useState(0)
 
+  const saveCart = (cart) => {
+    localStorage.setItem('cart', JSON.stringify(cart)) // save cart to local storage
+    let subt = 0
+    for (const item in cart) {
+      subt += cart[item].qty * cart[item].price
+    }
+    setSubTotal(subt)
+  }
 }
 
 export default MyApp
