676befd9b2560f8cbce54c65b6392fa2d350ad5e
Junaid Syed <sjunaid626@gmail.com>
2023-12-20 19:55:06 +0530

    Create the name and email fields

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,6 +5,18 @@ const checkout = ({ cart, addToCart, deleteItemFromCart, subTotal }) => {
         <h4 className="text-3xl text-center">Checkout</h4>
         <p className="text-md mr-auto">1. Fill your details.</p>
       </div>
+      <div className="w-full sm:w-1/2 flex flex-col sm:flex-row gap-4 justify-center">
+        <input
+          type="name"
+          placeholder="Enter Your Name"
+          className="w-full sm:w-3/4 bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-[#9a4747] focus:bg-transparent border border-gray-300 focus:border-[#9a4747] text-base outline-none text-gray-700 py-1 px-3"
+        />
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          className="w-full sm:w-3/4 bg-gray-100 bg-opacity-50 rounded focus:ring-2 focus:ring-[#9a4747] focus:bg-transparent border border-gray-300 focus:border-[#9a4747] text-base outline-none text-gray-700 py-1 px-3"
+        />
+      </div>
     </div>
   )
 }
