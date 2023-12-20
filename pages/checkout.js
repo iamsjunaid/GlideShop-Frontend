01e61d115b170ffa7635233544098771a99f0a79
Junaid Syed <sjunaid626@gmail.com>
2023-12-20 19:59:19 +0530

    Format the code structure

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,3 +1,8 @@
+import React from 'react'
+import Link from 'next/link'
+import { HiOutlinePlus, HiOutlineMinus } from 'react-icons/hi'
+import { FaCircleArrowRight } from 'react-icons/fa6'
+
 const checkout = ({ cart, addToCart, deleteItemFromCart, subTotal }) => {
   return (
     <div className="w-full flex flex-col items-center h-full gap-4 my-8 p-4">
