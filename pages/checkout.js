d8b4159b49a980552ff10c26e40c9c9a84c87608
Junaid Syed <sjunaid626@gmail.com>
2023-12-20 19:54:08 +0530

    Create the checkout page

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,7 @@
 const checkout = ({ cart, addToCart, deleteItemFromCart, subTotal }) => {
   return (
     <div className="w-full flex flex-col items-center h-full gap-4 my-8 p-4">
+    </div>
   )
 }
 
