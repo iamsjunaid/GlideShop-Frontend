8c70e4ea210dad703ca01be4b80e98e6854cc118
Junaid Syed <sjunaid626@gmail.com>
2023-12-20 19:54:33 +0530

    Create page headline

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,10 @@
 const checkout = ({ cart, addToCart, deleteItemFromCart, subTotal }) => {
   return (
     <div className="w-full flex flex-col items-center h-full gap-4 my-8 p-4">
+      <div className="w-full sm:w-1/2">
+        <h4 className="text-3xl text-center">Checkout</h4>
+        <p className="text-md mr-auto">1. Fill your details.</p>
+      </div>
     </div>
   )
 }
