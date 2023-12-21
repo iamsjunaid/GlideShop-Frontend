3bdb396c2c609c12e0fa868f78beb2a3dcb60652
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 16:36:19 +0530

    create heading and subheading

--- pages/orders.js
+++ pages/orders.js
@@ -4,6 +4,13 @@ const orders = () => {
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
         <div className="lg:w-4/5 mx-auto flex flex-wrap">
+          <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+            <h2 className="text-sm title-font text-[#9a4747] tracking-widest">
+              GLIDE SHOP
+            </h2>
+            <p className="text-gray-900 font-medium mb-4">
+              Thank you! Order placed successfully. We&apos;ll be delievering your order soon.
+            </p>
         </div>
       </div>
     </section>
