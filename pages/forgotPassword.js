fca83b5443a7653d77f21069b77dce94e1e8c8e2
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 21:02:50 +0530

    Create the forgot Password page

---
+++ pages/forgotPassword.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { IoArrowBackCircleOutline, IoLogInOutline } from 'react-icons/io5'
+import Link from 'next/link'
+
+const forgotPassword = () => {
+  return (
+    <div className="relative mx-auto w-4/5 sm:w-full my-4 max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+      <div className="w-full">
