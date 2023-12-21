db3a6ca428d5a0a532d4b16d807960f8e82b7c83
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 20:57:08 +0530

    Create the heading

--- pages/login.js
+++ pages/login.js
@@ -4,6 +4,12 @@ import Link from 'next/link'
 
 const login = () => {
   return (
+    <div className="relative mx-auto w-4/5 sm:w-full my-4 max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+      <div className="w-full">
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold text-gray-900">Sign in</h1>
+          <p className="mt-2 text-gray-500">Sign in below to access your account</p>
+        </div>
   )
 }
 
