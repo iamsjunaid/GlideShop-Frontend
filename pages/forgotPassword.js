f81d477605538a103af8a032a0c7136a9b354ad3
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 21:03:31 +0530

    Create email field

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -10,3 +10,21 @@ const forgotPassword = () => {
           <h1 className="text-3xl font-semibold text-gray-900">Forgot Password</h1>
           <p className="mt-2 text-gray-500">Recover your account</p>
         </div>
+        <div className="mt-5">
+          <form action="">
+            <div className="relative mt-6">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+                className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-[#9a4747] focus:outline-none"
+                autoComplete="NA"
+              />
+              <label
+                htmlFor="email"
+                className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800"
+              >
+                Email Address
+              </label>
+            </div>
