4168e4fc1b932b86c3f7c51d2a3e7b724b1d800f
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 16:34:41 +0530

    Create parent div

--- pages/orders.js
+++ pages/orders.js
@@ -3,6 +3,7 @@ const orders = () => {
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
+      </div>
     </section>
 
   )
