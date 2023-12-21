07dbb3e3ee2134f9b7cc5ed1a61100762d796603
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 20:57:23 +0530

    Create email field

--- pages/login.js
+++ pages/login.js
@@ -10,6 +10,17 @@ const login = () => {
           <h1 className="text-3xl font-semibold text-gray-900">Sign in</h1>
           <p className="mt-2 text-gray-500">Sign in below to access your account</p>
         </div>
+        <div className="mt-5">
+          <form action="">
+            <div className="relative mt-6">
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email Address"
+                className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-[#9a4747] focus:outline-none"
+                autoComplete="NA"
+              />
   )
 }
 
