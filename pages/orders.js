12f0bfbd008bdfea5710fc3bd4f4303915eb9db8
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 16:35:47 +0530

    Create wrapper

--- pages/orders.js
+++ pages/orders.js
@@ -3,6 +3,7 @@ const orders = () => {
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
+        <div className="lg:w-4/5 mx-auto flex flex-wrap">
         </div>
       </div>
     </section>
