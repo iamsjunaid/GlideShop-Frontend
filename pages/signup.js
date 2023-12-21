5631b924768e2779a0b87354a9b573ac492cb8e4
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 20:59:36 +0530

    Create signup page

---
+++ pages/signup.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { BiUserPlus } from 'react-icons/bi'
+import Link from 'next/link'
+
+const signup = () => {
+  return (
+    <div className="relative mx-auto w-4/5 sm:w-full my-4 max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
+      <div className="w-full">
