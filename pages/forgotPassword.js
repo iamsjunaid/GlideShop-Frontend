2c86bf00f491090c8ebce26ef5be46c5eb845c95
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 21:03:09 +0530

    Create the heading

--- pages/forgotPassword.js
+++ pages/forgotPassword.js
@@ -6,3 +6,7 @@ const forgotPassword = () => {
   return (
     <div className="relative mx-auto w-4/5 sm:w-full my-4 max-w-md bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:rounded-xl sm:px-10">
       <div className="w-full">
+        <div className="text-center">
+          <h1 className="text-3xl font-semibold text-gray-900">Forgot Password</h1>
+          <p className="mt-2 text-gray-500">Recover your account</p>
+        </div>
