e94cd56587ddc1f706969b0016e499a3e32ecf63
Junaid Syed <sjunaid626@gmail.com>
2023-12-21 16:30:31 +0530

    Remove duplicate items

--- pages/stickers.js
+++ pages/stickers.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const stickers = () => {
   return (
@@ -7,15 +8,17 @@ const stickers = () => {
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap -m-4 gap-16 justify-center">
           <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={420}
-                height={260}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
+            <Link href="product/[slug]" as="/product/some-product-slug">
+              <div className="block relative h-48 rounded overflow-hidden">
+                <Image
+                  src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
+                  alt="ecommerce"
+                  width={420}
+                  height={260}
+                  className="object-contain object-center w-full h-full block"
+                />
+              </div>
+            </Link>
             <div className="mt-4 text-center">
               <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
                 CATEGORY
@@ -24,146 +27,6 @@ const stickers = () => {
                 The Catalyzer
               </h2>
               <p className="mt-1">$16.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={421}
-                height={261}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Shooting Stars
-              </h2>
-              <p className="mt-1">$21.15</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={422}
-                height={262}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Neptune
-              </h2>
-              <p className="mt-1">$12.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={423}
-                height={263}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The 400 Blows
-              </h2>
-              <p className="mt-1">$18.40</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={424}
-                height={264}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The Catalyzer
-              </h2>
-              <p className="mt-1">$16.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={425}
-                height={265}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Shooting Stars
-              </h2>
-              <p className="mt-1">$21.15</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={426}
-                height={266}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                Neptune
-              </h2>
-              <p className="mt-1">$12.00</p>
-            </div>
-          </div>
-          <div className="lg:w-1/5 md:w-1/2 p-4 w-4/5 border border-gray-200 rounded-lg hover:shadow-lg">
-            <a className="block relative h-48 rounded overflow-hidden">
-              <Image
-                src="https://www.redwolf.in/image/cache/catalog/stickers/marvel-captain-america-the-shield-sticker-600x800.jpg"
-                alt="ecommerce"
-                width={427}
-                height={267}
-                className="object-contain object-center w-full h-full block"
-              />
-            </a>
-            <div className="mt-4 text-center">
-              <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-                CATEGORY
-              </h3>
-              <h2 className="text-gray-900 title-font text-lg font-medium">
-                The 400 Blows
-              </h2>
-              <p className="mt-1">$18.40</p>
             </div>
           </div>
         </div>
